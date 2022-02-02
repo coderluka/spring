822fa0d66cf98d1800224ffdd1f7e6416a2deb3e
Dimitrijevic Luka (POI - AT/Hagenberg) <coderluka>
2022-02-02 15:12:32 +0100

    inversion of control example

--- dependencyInjection/src/main/java/work/coderluka/dependencyinjection/DependencyInjectionApplication.java
+++ dependencyInjection/src/main/java/work/coderluka/dependencyinjection/DependencyInjectionApplication.java
@@ -2,12 +2,20 @@ package work.coderluka.dependencyinjection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import work.coderluka.dependencyinjection.controllers.MyController;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+        MyController myCtrl = (MyController) ctx.getBean("myController");
+
+        String greeting = myCtrl.sayHello();
+
+        System.out.println(greeting);
     }
 
 }
